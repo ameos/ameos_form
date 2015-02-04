c86ac8c0ffb6e50ae855c8819327e48b20bf268f
root <root@dev21.ameos.info>
2015-02-04 11:24:10 +0100

    Bug fixed on search with element radio and range

--- Classes/Elements/Radio.php
+++ Classes/Elements/Radio.php
@@ -37,4 +37,26 @@ class Radio extends ElementAbstract {
 		}
 		return $data;
 	}
+	
+	/**
+	 * return where clause
+	 *
+	 * @return	bool|array FALSE if no search. Else array with search type and value
+	 */
+	public function getClause() {
+		if($this->getValue() != '') {
+			if($this->overrideClause !== FALSE) {
+				return parent::getClause();
+			} else {
+				return [
+					'elementname'  => $this->getName(),
+					'elementvalue' => $this->getValue(),
+					'field' => $this->getSearchField(),
+					'type'  => 'equals',
+					'value' => $this->getValue()
+				];
+			}
+		}
+		return FALSE;
+	}
 }
