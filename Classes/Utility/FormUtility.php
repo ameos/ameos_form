ba5f34d77bca33148e5aebfeb3860b0d75dcfbae
root <root@dev21.ameos.info>
2015-03-31 17:25:18 +0200

    [FEATURE] add checksingle (yes/no) form element

--- Classes/Utility/FormUtility.php
+++ Classes/Utility/FormUtility.php
@@ -18,25 +18,26 @@ class FormUtility {
 	 */
 	public static function makeElementInstance($absolutename, $name, $type = '', $configuration, $form = FALSE) {
 		switch($type) {
-			case 'textarea':   $element = GeneralUtility::makeInstance('\\Ameos\\AmeosForm\\Elements\\Textarea',   $absolutename, $name, $configuration, $form); break;
-			case 'password':   $element = GeneralUtility::makeInstance('\\Ameos\\AmeosForm\\Elements\\Password',   $absolutename, $name, $configuration, $form); break;
-			case 'dropdown':   $element = GeneralUtility::makeInstance('\\Ameos\\AmeosForm\\Elements\\Dropdown',   $absolutename, $name, $configuration, $form); break;
-			case 'checkbox':   $element = GeneralUtility::makeInstance('\\Ameos\\AmeosForm\\Elements\\Checkbox',   $absolutename, $name, $configuration, $form); break;
- 			case 'submit':     $element = GeneralUtility::makeInstance('\\Ameos\\AmeosForm\\Elements\\Submit',     $absolutename, $name, $configuration, $form); break;
-			case 'hidden':     $element = GeneralUtility::makeInstance('\\Ameos\\AmeosForm\\Elements\\Hidden',     $absolutename, $name, $configuration, $form); break;
-			case 'button':     $element = GeneralUtility::makeInstance('\\Ameos\\AmeosForm\\Elements\\Button',     $absolutename, $name, $configuration, $form); break;
-			case 'upload':     $element = GeneralUtility::makeInstance('\\Ameos\\AmeosForm\\Elements\\Upload',     $absolutename, $name, $configuration, $form); break;
-			case 'radio':      $element = GeneralUtility::makeInstance('\\Ameos\\AmeosForm\\Elements\\Radio',      $absolutename, $name, $configuration, $form); break;
-			case 'date':       $element = GeneralUtility::makeInstance('\\Ameos\\AmeosForm\\Elements\\Date',       $absolutename, $name, $configuration, $form); break;
-			case 'datepicker': $element = GeneralUtility::makeInstance('\\Ameos\\AmeosForm\\Elements\\Datepicker', $absolutename, $name, $configuration, $form); break;
-			case 'time':       $element = GeneralUtility::makeInstance('\\Ameos\\AmeosForm\\Elements\\Time',       $absolutename, $name, $configuration, $form); break;
-			case 'timepicker': $element = GeneralUtility::makeInstance('\\Ameos\\AmeosForm\\Elements\\Timepicker', $absolutename, $name, $configuration, $form); break;
-			case 'captcha':    $element = GeneralUtility::makeInstance('\\Ameos\\AmeosForm\\Elements\\Captcha',    $absolutename, $name, $configuration, $form); break;
-			case 'label':      $element = GeneralUtility::makeInstance('\\Ameos\\AmeosForm\\Elements\\Label',      $absolutename, $name, $configuration, $form); break;
-			case 'color':      $element = GeneralUtility::makeInstance('\\Ameos\\AmeosForm\\Elements\\Color',      $absolutename, $name, $configuration, $form); break;
-			case 'range':      $element = GeneralUtility::makeInstance('\\Ameos\\AmeosForm\\Elements\\Range',      $absolutename, $name, $configuration, $form); break;
-			case 'number':     $element = GeneralUtility::makeInstance('\\Ameos\\AmeosForm\\Elements\\Number',     $absolutename, $name, $configuration, $form); break;
-			case 'email':      $element = GeneralUtility::makeInstance('\\Ameos\\AmeosForm\\Elements\\Email',      $absolutename, $name, $configuration, $form); break;
+			case 'textarea':    $element = GeneralUtility::makeInstance('\\Ameos\\AmeosForm\\Elements\\Textarea',    $absolutename, $name, $configuration, $form); break;
+			case 'password':    $element = GeneralUtility::makeInstance('\\Ameos\\AmeosForm\\Elements\\Password',    $absolutename, $name, $configuration, $form); break;
+			case 'dropdown':    $element = GeneralUtility::makeInstance('\\Ameos\\AmeosForm\\Elements\\Dropdown',    $absolutename, $name, $configuration, $form); break;
+			case 'checkbox':    $element = GeneralUtility::makeInstance('\\Ameos\\AmeosForm\\Elements\\Checkbox',    $absolutename, $name, $configuration, $form); break;
+			case 'checksingle': $element = GeneralUtility::makeInstance('\\Ameos\\AmeosForm\\Elements\\Checksingle', $absolutename, $name, $configuration, $form); break;
+ 			case 'submit':      $element = GeneralUtility::makeInstance('\\Ameos\\AmeosForm\\Elements\\Submit',      $absolutename, $name, $configuration, $form); break;
+			case 'hidden':      $element = GeneralUtility::makeInstance('\\Ameos\\AmeosForm\\Elements\\Hidden',      $absolutename, $name, $configuration, $form); break;
+			case 'button':      $element = GeneralUtility::makeInstance('\\Ameos\\AmeosForm\\Elements\\Button',      $absolutename, $name, $configuration, $form); break;
+			case 'upload':      $element = GeneralUtility::makeInstance('\\Ameos\\AmeosForm\\Elements\\Upload',      $absolutename, $name, $configuration, $form); break;
+			case 'radio':       $element = GeneralUtility::makeInstance('\\Ameos\\AmeosForm\\Elements\\Radio',       $absolutename, $name, $configuration, $form); break;
+			case 'date':        $element = GeneralUtility::makeInstance('\\Ameos\\AmeosForm\\Elements\\Date',        $absolutename, $name, $configuration, $form); break;
+			case 'datepicker':  $element = GeneralUtility::makeInstance('\\Ameos\\AmeosForm\\Elements\\Datepicker',  $absolutename, $name, $configuration, $form); break;
+			case 'time':        $element = GeneralUtility::makeInstance('\\Ameos\\AmeosForm\\Elements\\Time',        $absolutename, $name, $configuration, $form); break;
+			case 'timepicker':  $element = GeneralUtility::makeInstance('\\Ameos\\AmeosForm\\Elements\\Timepicker',  $absolutename, $name, $configuration, $form); break;
+			case 'captcha':     $element = GeneralUtility::makeInstance('\\Ameos\\AmeosForm\\Elements\\Captcha',     $absolutename, $name, $configuration, $form); break;
+			case 'label':       $element = GeneralUtility::makeInstance('\\Ameos\\AmeosForm\\Elements\\Label',       $absolutename, $name, $configuration, $form); break;
+			case 'color':       $element = GeneralUtility::makeInstance('\\Ameos\\AmeosForm\\Elements\\Color',       $absolutename, $name, $configuration, $form); break;
+			case 'range':       $element = GeneralUtility::makeInstance('\\Ameos\\AmeosForm\\Elements\\Range',       $absolutename, $name, $configuration, $form); break;
+			case 'number':      $element = GeneralUtility::makeInstance('\\Ameos\\AmeosForm\\Elements\\Number',      $absolutename, $name, $configuration, $form); break;
+			case 'email':       $element = GeneralUtility::makeInstance('\\Ameos\\AmeosForm\\Elements\\Email',       $absolutename, $name, $configuration, $form); break;
 			
 			default:
 			case 'text':       $element = GeneralUtility::makeInstance('\\Ameos\\AmeosForm\\Elements\\Text',       $absolutename, $name, $configuration, $form); break;
