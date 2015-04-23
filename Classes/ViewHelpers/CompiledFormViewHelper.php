ad36ead10e023d4fca3926356468dc01f29646c7
Ameos <typo3dev@ameos.com>
2015-04-23 15:05:24 +0200

    [TASK] Add license information on php file

--- Classes/ViewHelpers/CompiledFormViewHelper.php
+++ Classes/ViewHelpers/CompiledFormViewHelper.php
@@ -1,6 +1,18 @@
 <?php
-
 namespace Ameos\AmeosForm\ViewHelpers;
+
+/*
+ * This file is part of the TYPO3 CMS project.
+ *
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * The TYPO3 project - inspiring people to share!
+ */
 
 class CompiledFormViewHelper extends \TYPO3\CMS\Fluid\Core\ViewHelper\AbstractViewHelper {
 
