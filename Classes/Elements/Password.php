6aa2174122b2680f6a0275a80311c0c1dd284e4a
Stéphane Fillion <s.fillion@ameos.com>
2019-03-11 15:02:13 +0100

    [BUGFIX] Bug with password field and encrypt with TYPO3 9.5

--- Classes/Elements/Password.php
+++ Classes/Elements/Password.php
@@ -14,91 +14,91 @@ namespace Ameos\AmeosForm\Elements;
  * The TYPO3 project - inspiring people to share!
  */
  
-use TYPO3\CMS\Saltedpasswords\Utility\SaltedPasswordsUtility;
-use TYPO3\CMS\Saltedpasswords\Salt\SaltFactory;
+use TYPO3\CMS\Core\Crypto\PasswordHashing\PasswordHashFactory;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
 use Ameos\AmeosForm\Utility\Events;
 
 class Password extends ElementAbstract 
 {
-	
-	/**
-	 * @var bool $searchable searchable
-	 */
-	protected $searchable = false;
-	
-	/**
-	 * @constuctor
-	 *
-	 * @param	string	$absolutename absolutename
-	 * @param	string	$name name
-	 * @param	array	$configuration configuration
-	 * @param	\Ameos\AmeosForm\Form $form form
-	 */
-	public function __construct($absolutename, $name, $configuration = [], $form) 
-	{
-		parent::__construct($absolutename, $name, $configuration, $form);
-		$this->configuration['encrypt'] = isset($configuration['encrypt']) ? (bool)$configuration['encrypt'] : false;
-		$this->configuration['fill_value'] = isset($configuration['fill_value']) ? (bool)$configuration['fill_value'] : false;
-		$this->configuration['fill_value_iferror'] = isset($configuration['fill_value']) ? (bool)$configuration['fill_value_iferror'] : true;
-	}
-	
-	/**
-	 * form to html
-	 *
-	 * @return	string the html
-	 */
-	public function toHtml() {
-		$attributes = $this->getAttributes();
-		if ($this->configuration['fill_value']
-		  || ($this->form->isSubmitted() && !$this->form->isValid() && $this->configuration['fill_value_iferror'])) {
-			$attributes.= ' value="' . $this->getValue() . '"';
-		}
-		return '<input type="password" id="' . $this->getHtmlId() . '" name="' . $this->absolutename . '"' . $attributes . ' />';
-	}
+    
+    /**
+     * @var bool $searchable searchable
+     */
+    protected $searchable = false;
+    
+    /**
+     * @constuctor
+     *
+     * @param    string    $absolutename absolutename
+     * @param    string    $name name
+     * @param    array    $configuration configuration
+     * @param    \Ameos\AmeosForm\Form $form form
+     */
+    public function __construct($absolutename, $name, $configuration = [], $form) 
+    {
+        parent::__construct($absolutename, $name, $configuration, $form);
+        $this->configuration['encrypt'] = isset($configuration['encrypt']) ? (bool)$configuration['encrypt'] : true;
+        $this->configuration['fill_value'] = isset($configuration['fill_value']) ? (bool)$configuration['fill_value'] : false;
+        $this->configuration['fill_value_iferror'] = isset($configuration['fill_value']) ? (bool)$configuration['fill_value_iferror'] : true;
+    }
+    
+    /**
+     * form to html
+     *
+     * @return    string the html
+     */
+    public function toHtml()
+    {
+        $attributes = $this->getAttributes();
+        if ($this->configuration['fill_value']
+          || ($this->form->isSubmitted() && !$this->form->isValid() && $this->configuration['fill_value_iferror'])) {
+            $attributes.= ' value="' . $this->getValue() . '"';
+        }
+        return '<input type="password" id="' . $this->getHtmlId() . '" name="' . $this->absolutename . '"' . $attributes . ' />';
+    }
 
-	/**
-	 * set the value
-	 * 
-	 * @param	string	$value value
-	 * @return 	\Ameos\AmeosForm\Elements\ElementAbstract this
-	 */
-	public function setValue($value) 
-	{		
-		if ($this->configuration['encrypt']) {
-			Events::getInstance($this->form->getIdentifier())->registerEvent('form_is_valid', [$this, 'encryptPassword'], [
-				'password' => $value,
-			]);
-		}
-		
-		$this->valueSetted = true;
-		$this->value = $value;
+    /**
+     * set the value
+     * 
+     * @param    string    $value value
+     * @return     \Ameos\AmeosForm\Elements\ElementAbstract this
+     */
+    public function setValue($value) 
+    {        
+        if ($this->configuration['encrypt']) {
+            Events::getInstance($this->form->getIdentifier())->registerEvent('form_is_valid', [$this, 'encryptPassword'], [
+                'password' => $value,
+            ]);
+        }
+        
+        $this->valueSetted = true;
+        $this->value = $value;
 
-		if ($this->form !== false) {
-			if ($this->form->getMode() == 'crud/extbase' && $value != '') {
-				$method = 'set' . \Ameos\AmeosForm\Utility\StringUtility::camelCase($this->name);
-				if (method_exists($this->form->getModel(), $method)) {
-					$this->form->getModel()->$method($value);
-				}
-			}
+        if ($this->form !== false) {
+            if ($this->form->getMode() == 'crud/extbase' && $value != '') {
+                $method = 'set' . \Ameos\AmeosForm\Utility\StringUtility::camelCase($this->name);
+                if (method_exists($this->form->getModel(), $method)) {
+                    $this->form->getModel()->$method($value);
+                }
+            }
 
-			if ($this->form->getMode() == 'crud/classic' && $value != '') {
-				if ($this->form->hasData($this->name)) {
-					$this->form->setData($this->name, $value);
-				}
-			}
-		}
+            if ($this->form->getMode() == 'crud/classic' && $value != '') {
+                if ($this->form->hasData($this->name)) {
+                    $this->form->setData($this->name, $value);
+                }
+            }
+        }
 
-		return $this;
-	}
+        return $this;
+    }
 
-	/**
-	 * encrypt password
-	 * @param string $password password
-	 */
-	public function encryptPassword($password) {
-		if (SaltedPasswordsUtility::isUsageEnabled() && $password != '') {
-			$password = SaltFactory::getSaltingInstance(null)->getHashedPassword($password);
-			$this->setValue($password);
-		}
-	}
+    /**
+     * encrypt password
+     * @param string $password password
+     */
+    public function encryptPassword($password)
+    {
+        $hashInstance = GeneralUtility::makeInstance(PasswordHashFactory::class)->getDefaultHashInstance('FE');
+        $this->setValue($hashInstance->getHashedPassword($password));
+    }
 }
