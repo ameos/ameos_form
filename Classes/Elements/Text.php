288c5aacc569b5276ffc2a43b7737db5ac6de900
Ameos <typo3dev@ameos.com>
2020-03-10 10:56:38 +0100

    [FEATURE] - Added compatibility with ameos_form_zipcity_suggest for ameos_form Text fields

--- Classes/Elements/Text.php
+++ Classes/Elements/Text.php
@@ -18,6 +18,24 @@ class Text extends ElementAbstract
 {
 	
 	/**
+	 * @constuctor
+	 *
+	 * @param	string	$absolutename absolutename
+	 * @param	string	$name name
+	 * @param	array	$configuration configuration
+	 * @param	\Ameos\AmeosForm\Form $form form
+	 */
+	public function __construct($absolutename, $name, $configuration = [], $form) 
+	{
+		parent::__construct($absolutename, $name, $configuration, $form);
+		if(\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::isLoaded('ameos_form_zipcity_suggest'))
+		{
+			$this->pageRenderer->addCssFile('/typo3conf/ext/ameos_form_zipcity_suggest/Resources/Public/Css/CitySuggest.css');
+			$this->pageRenderer->addJsFooterFile('/typo3conf/ext/ameos_form_zipcity_suggest/Resources/Public/Javascript/CitySuggest.js');
+		}
+	}
+
+	/**
 	 * form to html
 	 *
 	 * @return	string the html
